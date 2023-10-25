1cc0db2b41be1516d4e082a3273cd8105f7c498b
ShutingZhao <shuting@nirmata.com>
2023-10-25 17:15:10 +0800

    add tests/kyverno-pss.js
    Signed-off-by: ShutingZhao <shuting@nirmata.com>

--- k6/tests/kyverno-pss.js
+++ k6/tests/kyverno-pss.js
@@ -21,6 +21,7 @@ export const options = {
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+    abortOnFail: true,
   },
 };
 
