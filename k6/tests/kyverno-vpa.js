894b1893b4e1f52306377bc076ee0cfa217ea4cf
swastik959 <Sswastik959@gmail.com>
2024-03-31 17:08:59 +0530

    added corrections
    Signed-off-by: swastik959 <Sswastik959@gmail.com>

--- k6/tests/kyverno-vpa.js
+++ k6/tests/kyverno-vpa.js
@@ -15,25 +15,51 @@ import {
 
 const baseUrl = buildKubernetesBaseUrl();
 const namespace = getTestNamespace();
+let vpaCreated = false;
+let vpaName = "";
 
 export default function () {
-  const podName = `test-${randomString(8)}`;
-  const vpa = generateVPA(podName);
+  if (!vpaCreated) {
+    vpaName = `vpa-${randomString(8)}`;
+    const vpa = generateVPA(vpaName);
     vpa.metadata.labels = {
-        app: "k6-test",
+      app: "k6-test",
     };
     const params = getParamsWithAuth();
     params.headers["Content-Type"] = "application/json";
 
-  const createVpaRes = http.post(
-    `${baseUrl}/apis/autoscaling.k8s.io/v1/namespaces/${namespace}/verticalpodautoscalers`,
-    JSON.stringify(vpa),
+    const createVpaRes = http.post(
+      `${baseUrl}/apis/autoscaling.k8s.io/v1/namespaces/${namespace}/verticalpodautoscalers`,
+      JSON.stringify(vpa),
+      params
+    );
+    check(createVpaRes, {
+      "verify response code of POST VPA is 201": (r) => r.status === 201,
+    });
+    vpaCreated = true;
+  }
+  const podName = `test-${randomString(8)}`;
+  const pod = generatePod(podName);
+  pod.metadata.labels = {
+    app: "k6-test",
+  };
+
+  pod.spec.containers[0].securityContext = {};
+
+  const params = getParamsWithAuth();
+  params.headers["Content-Type"] = "application/json";
+
+  const createRes = http.post(
+    `${baseUrl}/api/v1/namespaces/${namespace}/pods`,
+    JSON.stringify(pod),
     params
   );
-  check(createVpaRes, {
-    "verify response code of POST VPA is 201": (r) => r.status === 201,
+  check(createRes, {
+    "verify response code of POST is 400": (r) => r.status === 400,
   });
+
 }
+
 export function teardown() {}
 
 export function handleSummary(data) {
